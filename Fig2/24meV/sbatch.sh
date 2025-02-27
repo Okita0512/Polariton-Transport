79c7b34eb572e9e3ee2f7d10db093e14b63824a3
Wenxiang Ying <82459822+Okita0512@users.noreply.github.com>
2025-02-27 13:13:42 -0500

    Add files via upload

---
+++ Fig2/24meV/sbatch.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#SBATCH -p action
+#SBATCH -o output.log
+#SBATCH --mem=1GB
+#SBATCH --time=1-00:00:00
+#SBATCH --cpus-per-task=1
+#SBATCH --job-name=Transport
+
+time python3 savedata.py
