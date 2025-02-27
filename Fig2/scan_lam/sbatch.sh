b28ebc052d2ae96f932d949237bcf8e2191c4cdd
Wenxiang Ying <82459822+Okita0512@users.noreply.github.com>
2025-02-27 13:13:30 -0500

    Delete Fig2 directory

--- Fig2/scan_lam/sbatch.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#SBATCH -p action
-#SBATCH -o output.log
-#SBATCH --mem=1GB
-#SBATCH --time=1-00:00:00
-#SBATCH --cpus-per-task=1
-#SBATCH --job-name=Transport
-
-time python3 lam-dep.py
